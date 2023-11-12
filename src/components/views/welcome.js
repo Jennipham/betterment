a9ac366ee6d2199fd35a77d5d3a706ee4a444075
Jennifer Pham <jxp100@student.bham.ac.uk>
2023-11-12 02:06:16 +0000

    refactor: adding states for terms agreed

--- src/components/views/welcome.js
+++ src/components/views/welcome.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Header from './header';
-import { useLocation } from 'react-router-dom'; // Import useLocation hook
 import '../styles/Welcome.css';
 import Footer from './footer';
+import { useState } from 'react';
 
 const Welcome = () => {
-    const location = useLocation(); // Use useLocation hook to access the state
-    const user = location.state?.user || { fname: 'Guest' }; // Default to 'Guest' if user information is not available
+    const [agreed, setAgreed] = useState(false);
+
+    const handleTermsClick = () => {
+        setAgreed(true);
+    };
 
     return (
         <>
             <Header loggedIn={true} />
             <div className="welcome-page">
                 <div className="welcome-box">
-                    <h1 className="welcome-message">Welcome to BetterMent {user.fname}!</h1>
+                    <h1 className="welcome-message">Welcome to BetterMent!</h1>
                     <h1 className="terms-agree">Please accept if you agree to the <a href="/termsofuse" className='underline-link'>Terms and Conditions</a></h1>
                     <div className='profile-button'>
-                        <button>Accept</button>
+                        <button onClick={() => { handleTermsClick() }} className='accept'>Accept</button>
                     </div>
                 </div>
             </div>
