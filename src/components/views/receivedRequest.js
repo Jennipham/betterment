202b2d1198cb905dbb28a1d9b0b74de7561fbe57
Jennifer Pham <jxp100@student.bham.ac.uk>
2024-01-04 18:23:23 +0000

    refactor: formatting received requests

--- src/components/views/receivedRequest.js
+++ src/components/views/receivedRequest.js
@@ -7,13 +7,17 @@ import '../styles/ReceivedRequests.css';
 const ReceivedRequest = ({ request }) => {
     return (
         <div className="request-box-received">
+            <div className='icon-box'>
             <img className='request-profile-icon' src={profile} alt="Profile Icon" />
+                <a href="#profile-link" className='view-profile'>View Profile</a>
+            </div>
             <div className="received-profile-info">
                 <h3>{request.username}</h3>
-                <p>View Profile</p>
             </div>
+            <div className='action-buttons'>
             <img src={cross} alt="Reject" className="action-icon" />
-            <img src={tick} alt="Accept" className="action-icon" />
+                <img src={tick} alt="Accept" className="action-icon" />
+            </div>
         </div>
     );
 };
