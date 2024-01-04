e2c905ffab760fd08b2394f7a03f26b99b58978a
Jennifer Pham <jxp100@student.bham.ac.uk>
2024-01-04 18:33:41 +0000

    refactor: formatting sent requests

--- src/components/views/sentRequest.js
+++ src/components/views/sentRequest.js
@@ -6,12 +6,16 @@ import '../styles/SentRequests.css';
 const SentRequest = ({ request }) => {
     return (
         <div className="request-box-sent">
-            <img className='request-profile-icon' src={profile} alt="Profile Icon" />
+            <div className='icon-box'>
+                <img className='request-profile-icon' src={profile} alt="Profile Icon" />
+                <a href="#profile-link" className='view-profile'>View Profile</a>
+            </div>
             <div className="sent-profile-info">
                 <h3>{request.username}</h3>
-                <p>View Profile</p>
             </div>
-            <img src={cross} alt="Cancel" className="action-icon" />
+            <div className='action-buttons'>
+                <img src={cross} alt="Cancel" className="action-icon" />
+            </div>
         </div>
     );
 };
