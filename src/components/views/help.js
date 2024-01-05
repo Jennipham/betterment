29c8393702acd9b91371d6f58c1901dccde9d574
Jennifer Pham <jxp100@student.bham.ac.uk>
2024-01-05 00:30:12 +0000

    feat: adding faqs to help page

--- src/components/views/help.js
+++ src/components/views/help.js
@@ -5,12 +5,36 @@ import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../utils/header';
 import Footer from '../utils/footer';
+import Faqs from './faqs';
 import '../styles/Help.css';
 
 const Help = () => {
     const location = useLocation();
     const user = location.state?.user || { email: 'guest@example.com' };
     const [formMessage, setFormMessage] = useState('');
+
+    const faqData = [
+        {
+            question: "What is BetterMent?",
+            answer: "BetterMent is a platform that..."
+        },
+        {
+            question: "How do I get started?",
+            answer: "To sign up as a mentor, go to our registration page and fill out the necessary information."
+        },
+        {
+            question: "How does the matching algorithm work?",
+            answer: "The algorithm is..."
+        },
+        {
+            question: "Why Development Coaching?",
+            answer: "Development coaching..."
+        },
+        {
+            question: "What Development Areas are available?",
+            answer: "Areas available include..."
+        },
+    ];
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -46,23 +70,25 @@ const Help = () => {
         <>
             <Header loggedIn={true} className="header" />
 
-        <div className="help-page">
-            <h2 className="help-header">Contact Us</h2>
-            <div className="form-container">
+            <div className="help-page">
+                <div className='faqs-help'>
+                <Faqs faqData={faqData} />
+                </div>
+                <h2 className="help-header">Contact Us</h2>
+                <div className='contact-form'>
                 <form onSubmit={handleSubmit}>
-                    <label>
-                        Please leave any Feedback or Queries:
-                        <textarea
-                            value={formMessage}
-                            onChange={(e) => setFormMessage(e.target.value)}
-                        />
-                    </label>
-                    <button type="submit">Submit Form</button>
-                </form>
-            </div>
-            </div>
+                        <label className='contact-label'>
+                            Please leave any Feedback or Queries:
+                            <textarea
+                                value={formMessage}
+                                onChange={(e) => setFormMessage(e.target.value)}
+                            />
+                        </label>
+                        <button type="submit">Submit Form</button>
+                    </form>
+                </div>
+                </div>
             <Footer />
-
         </>
     );
 };
