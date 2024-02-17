e63dbb276f9cc08a62a09ba5394a7c2224e3feec
Jennifer Pham <jxp100@student.bham.ac.uk>
2024-02-17 15:04:08 +0000

    refactor: adding get started button to top sectoin

--- src/components/views/topSection.js
+++ src/components/views/topSection.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import top from '../images/TopImage.png'
 import '../styles/Top.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const TopSection = () => {
+
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/signup');
+      };
 
     return (
         <div className="top-section">
             <div className="text">
                 <h1>Connecting Employees to Executive <span className='ment'>Ment</span>ors</h1>
                 <h5>Project by: Jennifer Pham 2023</h5>
-
-
+                <button className='get-started-button' onClick={handleButtonClick}>Get Started!</button>
             </div> 
             <div className="image">
                 <img src= {top} alt="Top Section" />
