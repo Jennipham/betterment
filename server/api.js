f24e272e5d8bc3609661121cbcafab484c2ad933
Jennifer Pham <jxp100@student.bham.ac.uk>
2024-03-06 21:53:41 +0000

    refactor: commenting out test schedule

--- server/api.js
+++ server/api.js
@@ -1464,14 +1464,14 @@ cron.schedule('0 0 */14 * *', async () => {
 });
 
 // Minute then hour
-cron.schedule('17 10 * * *', async () => {
-    try {
-        console.log('Running matching process for @test.com accounts...');
-        await matchLogic("test.com"); // Pass "test.com" as the domain
-    } catch (error) {
-        console.error('Error during scheduled matching process:', error);
-    }
-});
+// cron.schedule('17 10 * * *', async () => {
+//     try {
+//         console.log('Running matching process for @test.com accounts...');
+//         await matchLogic("test.com"); // Pass "test.com" as the domain
+//     } catch (error) {
+//         console.error('Error during scheduled matching process:', error);
+//     }
+// });
 
 const calculateNextMatchDay = () => {
     const today = new Date();
